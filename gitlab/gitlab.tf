f42fe9c008116cd94d579df792dc01f6f7fdf630
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2019-08-09 00:23:25 -0500

    Update gitlab.tf

--- gitlab/gitlab.tf
+++ gitlab/gitlab.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "${var.region}"
-}
-
 resource "aws_instance" "gitlab" {
   instance_type               = "${var.instance_type}"
   ami                         = "${var.ami}"
