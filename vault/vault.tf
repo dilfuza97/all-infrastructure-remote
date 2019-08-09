a84e15b6573ab1e29032108a472bdd5e650df7bc
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2019-08-08 23:32:50 -0500

    Update vault.tf

--- vault/vault.tf
+++ vault/vault.tf
@@ -1,19 +1,3 @@
-resource "aws_instance" "vault" {
-  instance_type               = "${var.instance_type}"
-  ami                         = "${var.ami}"
-  key_name                    = "${var.key_name}"
-  associate_public_ip_address = "true"
-  security_groups             = ["allow_ssh_and_vault"]
-
-  provisioner "file" {
-    source      = "vault.service"
-    destination = "/tmp/vault.service"
-
-    connection {
-      host        = "${self.public_ip}"
-      type        = "ssh"
-      user        = "${var.user}"
-      private_key = "${file(var.ssh_key_location)}"
   resource "aws_instance" "vault" {
   instance_type               = "${var.instance_type}"
   ami                         = "${var.ami}"
